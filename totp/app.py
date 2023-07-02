d7bdde3984576bc734227f239cb060c63e6ce51b
meirdev <maornet@gmail.com>
2023-07-02 20:10:40 +0300

    use verify

--- totp/app.py
+++ totp/app.py
@@ -177,7 +177,7 @@ async def two_factor(
 
     totp = pyotp.TOTP(user["totp_secret"])
 
-    if data.code != totp.now():
+    if totp.verify(data.code):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Invalid 2FA code",
